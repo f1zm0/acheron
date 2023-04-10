88edc76abe22fe93c4fa15b3ebf0f0de25526573
f1zm0 <root@fzm.ooo>
2023-04-10 19:31:47 +0200

    chore: move lib functions to entrypoint file

--- acheron.go
+++ acheron.go
@@ -1,6 +1,9 @@
 package acheron
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/f1zm0/acheron/internal/resolver"
 	"github.com/f1zm0/acheron/internal/resolver/rvasort"
 	"github.com/f1zm0/acheron/pkg/hashing"
@@ -17,6 +20,14 @@ type (
 
 type options struct {
 	hasher hashing.HashFunction
+}
+
+// WithHashFunction returns an Option that sets a custom hashing (or obfuscation)
+// function that will be used when resolving native api procedures by hash.
+func WithHashFunction(f hashing.HashFunction) Option {
+	return func(o *options) {
+		o.hasher = f
+	}
 }
 
 // New returns a new Acheron instance that can be used as a proxy to perform
@@ -38,10 +49,19 @@ func New(opts ...Option) (*Acheron, error) {
 	}
 }
 
-// WithHashFunction returns an Option that sets a custom hashing (or obfuscation)
-// function that will be used when resolving native api procedures by hash.
-func WithHashFunction(f hashing.HashFunction) Option {
-	return func(o *options) {
-		o.hasher = f
+// Syscall executes a syscall with the given function hash and arguments.
+// Returns the error code and an error if the syscall failed.
+func (a *Acheron) Syscall(fnHash int64, args ...uintptr) error {
+	sys, err := a.resolver.GetSyscall(fnHash)
+	if err != nil {
+		return err
 	}
+	if errCode := execIndirectSyscall(sys.SSN, sys.TrampolineAddr, args...); errCode != 0 {
+		return errors.New(fmt.Sprintf("syscall failed with error code %d", errCode))
+	}
+	return nil
 }
+
+// execIndirectSyscall function signature for go-asm impelementation.
+// returns 0 if the syscall was successful or an error code if the operation failed.
+func execIndirectSyscall(ssn uint16, gateAddr uintptr, argh ...uintptr) (errcode uint32)
