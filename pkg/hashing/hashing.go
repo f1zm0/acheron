428e9ba6a5ea0273babff63c2a7c3e9340ae5c4c
f1zm0 <root@fzm.ooo>
2023-04-20 14:47:31 +0200

    chore: add comments to exported functions

--- pkg/hashing/hashing.go
+++ pkg/hashing/hashing.go
@@ -1,7 +1,9 @@
 package hashing
 
+// HashFunction is a type alias for a function that takes a byte slice and returns a uint64.
 type HashFunction func([]byte) uint64
 
+// DJB2 is an implementation of the djb2 hash function. Ref: http://www.cse.yorku.ca/~oz/hash.html
 func DJB2(s []byte) uint64 {
 	var hash uint64 = 5381
 	for _, c := range s {
