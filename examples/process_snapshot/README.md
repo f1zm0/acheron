5ac8f2a4fc0d68d7fc247bcf0c160b4e80059829
f1zm0 <root@fzm.ooo>
2023-04-20 14:47:51 +0200

    feat: add process snapshot example using acheron

---
+++ examples/process_snapshot/README.md
@@ -0,0 +1,7 @@
+# Process Snapshot Example
+
+Compile with:
+
+```bash
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o process_snapshot.exe process_snapshot.go
+```
---
+++ examples/process_snapshot/main.go
@@ -0,0 +1,58 @@
+//go:build windows
+// +build windows
+
+package main
+
+import (
+	"unsafe"
+
+	"github.com/f1zm0/acheron"
+	"golang.org/x/sys/windows"
+)
+
+const nullptr = uintptr(0)
+
+func main() {
+	bufferSize := uint32(0)
+
+	// creates Acheron instance, resolves SSNs, collects clean trampolines in ntdll.dlll, etc.
+	acheron, err := acheron.New()
+	if err != nil {
+		panic(err)
+	}
+
+	// make indirect syscall for NtQuerySystemInformation to figure out the buffer size
+	_ = acheron.Syscall(
+		acheron.HashString("NtQuerySystemInformation"), // function name hash
+		0x5,                                  // _In_ SYSTEM_INFORMATION_CLASS SystemInformationClass
+		0,                                    // _Out_ PVOID SystemInformation
+		uintptr(bufferSize),                  // _In_ ULONG SystemInformationLength
+		uintptr(unsafe.Pointer(&bufferSize)), // _Out_opt_ PULONG ReturnLength
+	)
+
+	buf := make([]byte, bufferSize)
+
+	// make indirect syscall to get the process list (in buf)
+	if err := acheron.Syscall(
+		acheron.HashString("NtQuerySystemInformation"),
+		0x5, // SystemProcessInformation
+		uintptr(unsafe.Pointer(&buf[0])),
+		uintptr(bufferSize),
+		nullptr,
+	); err != nil {
+		panic(err)
+	}
+
+	// print each process ID and Name
+	offset := 0
+	for {
+		p := (*windows.SYSTEM_PROCESS_INFORMATION)(unsafe.Pointer(&buf[offset]))
+		if p.ImageName.String() != "" {
+			println("PID: ", p.UniqueProcessID, " Name: ", p.ImageName.String())
+		}
+		if p.NextEntryOffset == 0 {
+			break
+		}
+		offset += int(p.NextEntryOffset)
+	}
+}
