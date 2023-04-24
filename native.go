4d573287d18a9be0a41d3f917df06be659783931
f1zm0 <root@fzm.ooo>
2023-04-24 22:43:51 +0200

    feat: add helpers for errors

---
+++ native.go
@@ -0,0 +1,10 @@
+package acheron
+
+// Ref: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/87fba13e-bf06-450e-83b1-9241dc81e781
+const (
+	ErrSyscallNotFound = ((3 << 30) | (1 << 29) | 1) // Severity: STATUS_SEVERITY_ERROR, Custom: 1, Code: 1
+)
+
+func NT_SUCCESS(x uint32) bool {
+	return (x)&(1<<31) != 0 // error if severity bit (MSB) is 0x1
+}
